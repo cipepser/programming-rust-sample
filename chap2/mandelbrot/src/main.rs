62bb8538b110e84fe2bfa3cf5ba8ba8e54d77268
cipepser <respepic@gmail.com>
2018-08-18 12:28:14 +0900

    implement write_image

--- chap2/mandelbrot/src/main.rs
+++ chap2/mandelbrot/src/main.rs
@@ -96,3 +96,11 @@ fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, l
         }
     }
 }
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
+}
