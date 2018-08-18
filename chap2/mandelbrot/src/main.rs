aea631d1238c2e203e4b7a95273699176de91cec
cipepser <respepic@gmail.com>
2018-08-18 12:22:22 +0900

    implement render

--- chap2/mandelbrot/src/main.rs
+++ chap2/mandelbrot/src/main.rs
@@ -1,7 +1,11 @@
 extern crate num;
+extern crate image;
 
 use num::Complex;
 use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
 
 #[allow(dead_code)]
 fn complex_square_add_loop(c: Complex<f64>) {
@@ -78,23 +82,17 @@ fn test_pixel_to_point() {
     ), Complex { re: -0.5, im: -0.5 });
 }
 
+fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
+    assert_eq!(pixels.len(), bounds.0 * bounds.1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] =
+                match escape_time(point, 255) {
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                };
+        }
+    }
+}
