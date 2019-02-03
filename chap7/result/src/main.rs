7a118f654728fad01bfa7baaf4498604523a4bd1
cipepser <respepic@gmail.com>
2019-02-03 11:48:52 +0900

    add get_weather with main

---
+++ chap7/result/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let hometown = "Tokyo";
+    match get_weather(hometown) {
+        Ok(report) => {
+            println!("{}: {}", hometown, report);
+        }
+        Err(err) => {
+            println!("error querying the weather: {}", err);
+        }
+    }
+}
+
+fn get_weather(hometown: &str) -> Result<&str, String> {
+    match hometown {
+        "Tokyo" => Ok("Sunny!"),
+        _ => Err("no location".to_owned()),
+    }
+}
