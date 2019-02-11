c7fd9d79304fb414e932df7cbb539367ddbc3105
cipepser <respepic@gmail.com>
2019-02-11 09:08:48 +0900

    transfer program to library

---
+++ chap8/fern_sim/src/bin/efern.rs
@@ -0,0 +1,12 @@
+extern crate fern_sim;
+use fern_sim::{Fern, run_simulation};
+
+fn main() {
+    let mut fern = Fern {
+        size: 1.0,
+        growth_rate: 0.0001,
+    };
+
+    run_simulation(&mut fern, 1000);
+    println!("final fernn size: {}", fern.size);
+}
