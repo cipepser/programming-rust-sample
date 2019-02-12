32566f70293a1f2119b3b59bb99e600946da7bd4
cipepser <respepic@gmail.com>
2019-02-13 08:41:46 +0900

    add Broom

--- chap9/src/main.rs
+++ chap9/src/main.rs
@@ -4,18 +4,66 @@ struct GrayscaleMap {
     size: (usize, usize),
 }
 
-fn main() {
-    let width = 1024;
-    let height = 576;
-    let image = GrayscaleMap {
-        pixels: vec![0; width * height],
-        size: (width, height),
-    };
-    assert_eq!(image.size, (1024, 576));
-    assert_eq!(image.pixels.len(), 1024 * 576);
-}
-
 fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap {
     assert_eq!(pixels.len(), size.0 * size.1);
     GrayscaleMap { pixels, size }
+}
+
+struct Broom {
+    name: String,
+    height: u32,
+    health: u32,
+    position: (f32, f32, f32),
+    intent: BroomIntent,
+}
+
+/// Two possible alternatives for what a `Broom` could be working on.
+#[derive(Copy, Clone)]
+enum BroomIntent { FetchWater, DumpWater }
+
+// Receive the input room by value, taking ownership.
+fn chop(b: Broom) -> (Broom, Broom) {
+    // Initialize `broom1` mostly from `b`, changing only `height`.
+    // Since `String` is not `Copy`, `broom1` takes ownership of `b`'s name.
+    let mut broom1 = Broom { height: b.height / 2, ..b };
+
+    // Initialize `broom2` mostly from `broom1`.
+    // Since `String` is not `Copy`, we must clone `name` explicitly.
+    let mut broom2 = Broom {
+        name: broom1.name.clone(),
+        ..broom1
+    };
+
+    broom1.name.push_str(" I");
+    broom2.name.push_str(" II");
+
+    (broom1, broom2)
+}
+
+fn main() {
+//    // GrayscaleMap
+//    let width = 1024;
+//    let height = 576;
+//    let image = GrayscaleMap {
+//        pixels: vec![0; width * height],
+//        size: (width, height),
+//    };
+//    assert_eq!(image.size, (1024, 576));
+//    assert_eq!(image.pixels.len(), 1024 * 576);
+
+    // Broom
+    let hokey = Broom {
+        name: "Hokey".to_string(),
+        height: 60,
+        health: 100,
+        position: (100.0, 200.0, 0.0),
+        intent: BroomIntent::FetchWater,
+    };
+
+    let (hokey1, hokey2) = chop(hokey);
+    assert_eq!(hokey1.name, "Hokey I");
+    assert_eq!(hokey1.health, 100);
+
+    assert_eq!(hokey2.name, "Hokey II");
+    assert_eq!(hokey2.health, 100);
 }
