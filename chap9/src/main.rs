9c66c12398da48e9d347739fdb482066fde470d6
cipepser <respepic@gmail.com>
2019-02-12 09:52:02 +0900

    use struct expression

--- chap9/src/main.rs
+++ chap9/src/main.rs
@@ -3,3 +3,12 @@ struct GrayscaleMap {
     pixels: Vec<u8>,
     size: (usize, usize),
 }
+
+fn main() {
+    let width = 1024;
+    let height = 576;
+    let image = GrayscaleMap {
+        pixels: vec![0; width * height],
+        size: (width, height),
+    };
+}
