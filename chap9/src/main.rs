0f0e505b4b7800556cfbf390a6bee7228c825867
cipepser <respepic@gmail.com>
2019-02-13 09:24:15 +0900

    add Queue

--- chap9/src/main.rs
+++ chap9/src/main.rs
@@ -40,6 +40,54 @@ fn chop(b: Broom) -> (Broom, Broom) {
     (broom1, broom2)
 }
 
+/// A first-in, first-out queue of characters.
+pub struct Queue {
+    older: Vec<char>,
+    // older elements, eldest last.
+    younger: Vec<char>, // younger element, youngest last.
+}
+
+impl Queue {
+    /// Push a character onto the back of a queue.
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    /// Pop a character off the front of a queue. Return `Some(c)` if there
+    /// was a character to pop, or `None` if the queue was empty.
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            // Bring the elements in youger over to older, and put them in the promised order.
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+
+        // Now older is guaranteed to have something. Vec's pop method
+        // already returns an Option, so we're set.
+        self.older.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<char>, Vec<char>) {
+        (self.older, self.younger)
+    }
+
+    pub fn new() -> Queue {
+        Queue {
+            older: Vec::new(),
+            younger: Vec::new(),
+        }
+    }
+}
+
 fn main() {
 //    // GrayscaleMap
 //    let width = 1024;
@@ -51,19 +99,50 @@ fn main() {
 //    assert_eq!(image.size, (1024, 576));
 //    assert_eq!(image.pixels.len(), 1024 * 576);
 
-    // Broom
-    let hokey = Broom {
-        name: "Hokey".to_string(),
-        height: 60,
-        health: 100,
-        position: (100.0, 200.0, 0.0),
-        intent: BroomIntent::FetchWater,
-    };
+//    // Broom
+//    let hokey = Broom {
+//        name: "Hokey".to_string(),
+//        height: 60,
+//        health: 100,
+//        position: (100.0, 200.0, 0.0),
+//        intent: BroomIntent::FetchWater,
+//    };
+//
+//    let (hokey1, hokey2) = chop(hokey);
+//    assert_eq!(hokey1.name, "Hokey I");
+//    assert_eq!(hokey1.health, 100);
+//
+//    assert_eq!(hokey2.name, "Hokey II");
+//    assert_eq!(hokey2.health, 100);
 
-    let (hokey1, hokey2) = chop(hokey);
-    assert_eq!(hokey1.name, "Hokey I");
-    assert_eq!(hokey1.health, 100);
+    // Queue
+//    let mut q = Queue {
+//        older: Vec::new(),
+//        younger: Vec::new(),
+//    };
+    let mut q = Queue::new();
 
-    assert_eq!(hokey2.name, "Hokey II");
-    assert_eq!(hokey2.health, 100);
+    q.push('0');
+    q.push('1');
+    assert_eq!(q.pop(), Some('0'));
+
+    q.push('∞');
+    assert_eq!(q.pop(), Some('1'));
+    assert_eq!(q.pop(), Some('∞'));
+    assert_eq!(q.pop(), None);
+
+    assert!(q.is_empty());
+    q.push('x');
+    assert!(!q.is_empty());
+    q.pop();
+
+    q.push('P');
+    q.push('D');
+    assert_eq!(q.pop(), Some('P'));
+    q.push('X');
+
+    let (older, younger) = q.split();
+    // q is now uninitialized.
+    assert_eq!(older, vec!['D']);
+    assert_eq!(younger, vec!['X']);
 }
