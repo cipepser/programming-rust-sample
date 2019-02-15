9c706a0602bf8fe986050301c2c3244950c85f4c
cipepser <respepic@gmail.com>
2019-02-15 11:46:47 +0900

    add http_status_from_u32

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -5,10 +5,20 @@ enum Pet {
     Giraffe,
 }
 
+#[derive(Debug, PartialEq)]
 enum HttpStatus {
     Ok = 200,
     NotModified = 304,
     NotFound = 404,
+}
+
+fn http_status_from_u32(n: u32) -> Option<HttpStatus> {
+    match n {
+        200 => Some(HttpStatus::Ok),
+        304 => Some(HttpStatus::NotModified),
+        404 => Some(HttpStatus::NotFound),
+        _ => None
+    }
 }
 
 fn main() {
@@ -16,4 +26,6 @@ fn main() {
     assert_eq!(size_of::<HttpStatus>(), 2); // 404 doesn't fit in a u8
 
     assert_eq!(HttpStatus::Ok as i32, 200);
+
+    assert_eq!(http_status_from_u32(200).unwrap(), HttpStatus::Ok);
 }
