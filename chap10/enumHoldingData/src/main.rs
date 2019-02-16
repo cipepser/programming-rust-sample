279f6f048f9f0f9358000cebade67d2fe6667a61
cipepser <respepic@gmail.com>
2019-02-16 10:39:38 +0900

    change singular and plural

--- chap10/enumHoldingData/src/main.rs
+++ chap10/enumHoldingData/src/main.rs
@@ -38,18 +38,18 @@ impl TimeUnit {
 
 fn rough_time_to_english(rt: RoughTime) -> String {
     match rt {
-        RoughTime::InThePast(units, 1) => format!("a {} ago", units.plural()),
-        RoughTime::InThePast(units, count) => format!("{} {} ago", count, units.singular()),
+        RoughTime::InThePast(units, 1) => format!("a {} ago", units.singular()),
+        RoughTime::InThePast(units, count) => format!("{} {} ago", count, units.plural()),
         RoughTime::JustNow => format!("just now"),
-        RoughTime::InTHeFuture(units, 1) => format!("a {} from now", units.plural()),
-        RoughTime::InTHeFuture(units, count) => format!("{} {} from now", count, units.singular()),
+        RoughTime::InTHeFuture(units, 1) => format!("a {} from now", units.singular()),
+        RoughTime::InTHeFuture(units, count) => format!("{} {} from now", count, units.plural()),
     }
 }
 
 fn main() {
     let four_score_and_seven_years_ago = RoughTime::InThePast(TimeUnit::Years, 4 * 20 + 7);
     println!("{:?}", rough_time_to_english(four_score_and_seven_years_ago));
-
+    
     let three_hours_from_now = RoughTime::InThePast(TimeUnit::Hours, 3);
     println!("{:?}", rough_time_to_english(three_hours_from_now));
 }
