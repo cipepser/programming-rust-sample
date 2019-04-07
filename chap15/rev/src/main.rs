99d1bd0f5491ace8918fd434d09feae58d0d1c8a
cipepser <respepic@gmail.com>
2019-04-07 10:22:05 +0900

    add rev

---
+++ chap15/rev/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    use std::iter::DoubleEndedIterator;
+
+    let bee_parts = ["head", "thorax", "abdomen"];
+
+    let mut iter = bee_parts.iter();
+    assert_eq!(iter.next(), Some(&"head"));
+    assert_eq!(iter.next_back(), Some(&"abdomen"));
+    assert_eq!(iter.next(), Some(&"thorax"));
+
+    assert_eq!(iter.next_back(), None);
+    assert_eq!(iter.next(), None);
+}
