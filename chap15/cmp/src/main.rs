7f21c62f9e9c4092471d223bc9569f004d0b3f61
cipepser <respepic@gmail.com>
2019-04-14 12:44:25 +0900

    compare chars

--- chap15/cmp/src/main.rs
+++ chap15/cmp/src/main.rs
@@ -22,4 +22,14 @@ fn main() {
 
     assert_eq!(populations.iter().max_by_key(|&(_name, pop)| pop), Some((&"Portland", &583_776)));
     assert_eq!(populations.iter().min_by_key(|&(_name, pop)| pop), Some((&"Greenhorn", &2)));
+
+    let packed = "Helen of Troy";
+    let spaced = "Helen   of   Troy";
+    let obscure = "Helen of Sandusky";
+
+    assert!(packed != spaced);
+    assert!(packed.split_whitespace().eq(spaced.split_whitespace()));
+
+    assert!(spaced < obscure);
+    assert!(spaced.split_whitespace().gt(obscure.split_whitespace()));
 }
