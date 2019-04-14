79145f82dd4f412d22ba69ba7c5dae1764eca72f
cipepser <respepic@gmail.com>
2019-04-14 13:57:25 +0900

    add my_iterator

---
+++ chap15/my_iterator/src/main.rs
@@ -0,0 +1,121 @@
+struct I32Range {
+    start: i32,
+    end: i32,
+}
+
+impl Iterator for I32Range {
+    type Item = i32;
+    fn next(&mut self) -> Option<i32> {
+        if self.start >= self.end {
+            return None;
+        }
+        let result = Some(self.start);
+        self.start += 1;
+        result
+    }
+}
+
+enum BinaryTree<T> {
+    Empty,
+    NonEmpty(Box<TreeNode<T>>),
+}
+
+struct TreeNode<T> {
+    element: T,
+    left: BinaryTree<T>,
+    right: BinaryTree<T>,
+}
+
+use self::BinaryTree::*;
+
+// The state of an in-order traversal of a `BinaryTree`.
+
+struct TreeIter<'a, T: 'a> {
+    // A stack of references to tree nodes. Since we use `Vec`'s
+    // `push` and `pop` methods, the top of the stack is the end of the vector.
+
+    // The node the iterator will visit next is at the top of the stack,
+    // with those ancestors still unvisited below it. If the stack is empty,
+    // the iterator is over.
+    unvisited: Vec<&'a TreeNode<T>>,
+}
+
+impl<'a, T: 'a> TreeIter<'a, T> {
+    fn push_left_edge(&mut self, mut tree: &'a BinaryTree<T>) {
+        while let NonEmpty(ref node) = *tree {
+            self.unvisited.push(node);
+            tree = &node.left;
+        }
+    }
+}
+
+impl<T> BinaryTree<T> {
+    fn iter(&self) -> TreeIter<T> {
+        let mut iter = TreeIter { unvisited: Vec::new() };
+        iter.push_left_edge(self);
+        iter
+    }
+}
+
+impl<'a, T: 'a> IntoIterator for &'a BinaryTree<T> {
+    type Item = &'a T;
+    type IntoIter = TreeIter<'a, T>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, T> Iterator for TreeIter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<&'a T> {
+        // Find the node this iterator must produce,
+        // or finish the iteration.
+        let node = match self.unvisited.pop() {
+            None => return None,
+            Some(n) => n,
+        };
+
+        // The next node after this one is the leftmost child of
+        // this node's right child, so push the path from here down.
+        self.push_left_edge(&node.right);
+
+        // Produce a reference to this node's value.
+        Some(&node.element)
+    }
+}
+
+fn main() {
+    let mut pi = 0.0;
+    let mut numerator = 1.0;
+
+    for k in (I32Range { start: 0, end: 14 }) {
+        pi += numerator / (2 * k + 1) as f64;
+        numerator /= -3.0;
+    }
+    pi *= f64::sqrt(12.0);
+
+    // IEEE 754 specifies this result exactly;
+    assert_eq!(pi as f32, std::f32::consts::PI);
+
+    fn make_node<T>(left: BinaryTree<T>, element: T, right: BinaryTree<T>) -> BinaryTree<T> {
+        NonEmpty(Box::new(TreeNode { left, element, right }))
+    }
+
+    // Build a small tree.
+    let subtree_l = make_node(Empty, "mecha", Empty);
+    let subtree_rl = make_node(Empty, "droid", Empty);
+    let subtree_r = make_node(subtree_rl, "robot", Empty);
+    let tree = make_node(subtree_l, "Jaeger", subtree_r);
+
+    // Iterate over it.
+    let mut v = Vec::new();
+    for kind in &tree {
+        v.push(*kind);
+    }
+    assert_eq!(v, ["mecha", "Jaeger", "droid", "robot"]);
+
+    assert_eq!(tree.iter()
+                   .map(|name| format!("mega-{}", name))
+                   .collect::<Vec<_>>(),
+               vec!["mega-mecha", "mega-Jaeger", "mega-droid", "mega-robot"]);
+}
