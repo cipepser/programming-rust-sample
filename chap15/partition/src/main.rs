a9de3fd73bd4e7b65e3bce0c4b861d01481a713d
cipepser <respepic@gmail.com>
2019-04-14 13:21:24 +0900

    add partition

---
+++ chap15/partition/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let things = ["doorknob", "mushroom", "noodle", "giraffe", "grapefruit"];
+
+    let (living, nonliving): (Vec<&str>, Vec<&str>) =
+        things.iter().partition(|name|
+            name.as_bytes()[0] & 1 != 0
+        );
+
+    assert_eq!(living, vec!["mushroom", "giraffe", "grapefruit"]);
+    assert_eq!(nonliving, vec!["doorknob", "noodle"]);
+}
