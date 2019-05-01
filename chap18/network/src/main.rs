a74cb16a3ce18a2aad605e7db89bf25cb58bdc62
cipepser <respepic@gmail.com>
2019-05-01 16:52:36 +0900

    implement http_get_main

--- chap18/network/src/main.rs
+++ chap18/network/src/main.rs
@@ -1,5 +1,8 @@
+extern crate reqwest;
+
 use std::net::TcpListener;
-use std::io;
+use std::io::{self, Write};
+use std::error::Error;
 use std::thread::spawn;
 
 fn echo_main(addr: &str) -> io::Result<()> {
@@ -18,6 +21,27 @@ fn echo_main(addr: &str) -> io::Result<()> {
     }
 }
 
+fn http_get_main(url: &str) -> Result<(), Box<Error>> {
+    let mut response = reqwest::get(url)?;
+    if !response.status().is_success() {
+        Err(format!("{}", response.status()))?;
+    }
+
+    let stdout = io::stdout();
+    io::copy(&mut response, &mut stdout.lock())?;
+
+    Ok(())
+}
+
 fn main() {
-    echo_main("127.0.0.1:17007").expect("error: ");
+//    echo_main("127.0.0.1:17007").expect("error: ");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        writeln!(io::stderr(), "usage: http-get URL").unwrap();
+        return;
+    }
+
+    if let Err(err) = http_get_main(&args[1]) {
+        writeln!(io::stderr(), "error: {}", err).unwrap();
+    }
 }
