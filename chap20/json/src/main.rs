718ff5a554039a223a2800db3e41c8636afddde7
cipepser <respepic@gmail.com>
2020-01-03 13:27:46 +0900

    add Json::Array and Json::Object

--- chap20/json/src/main.rs
+++ chap20/json/src/main.rs
@@ -17,10 +17,38 @@ enum Json {
 macro_rules! json {
     (null) => {
         Json::Null
+    };
+    ([ $( $element:tt ),* ]) => {
+        Json::Array(vec![ $( json!($element) ),* ])
+    };
+    ([ $( $key:tt : $value:tt ),* ]) => {
+        Json::Object(Box::new(vec![
+            $( $key.to_string(), json!($value) ),*
+        ].into_iter().collect()))
+    };
+    ($other:tt) => {
+        ... // TODO: Return Number, String or Boolean
     }
 }
 
 #[test]
 fn json_null() {
     assert_eq!(json!(null), Json::Null);
+}
+
+#[test]
+fn json_array_with_json_element() {
+    let macro_generated_value = json!(
+        [
+            {
+                "pitch": 440.0
+            }
+        ]
+    );
+    let hand_coded_value = Json::Array(vec![
+        Json::Object(Box::new(vec![
+            ("pitch".to_string(), Json::Number(440.0))
+        ].collect()))
+    ]);
+    assert_eq!(macro_generated_value, hand_coded_value);
 }
