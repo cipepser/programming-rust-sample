08c39241f6e95a33bdff5641407f906f7e56d689
cipepser <respepic@gmail.com>
2020-01-03 13:01:21 +0900

    add Json::Null

--- chap20/json/src/main.rs
+++ chap20/json/src/main.rs
@@ -1,3 +1,26 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Clone, PartialEq, Debug)]
+enum Json {
+    Null,
+    Boolean(bool),
+    Number(f64),
+    String(String),
+    Array(Vec<Json>),
+    Object(Box<HashMap<String, Json>>),
+}
+
+macro_rules! json {
+    (null) => {
+        Json::Null
+    }
+}
+
+#[test]
+fn json_null() {
+    assert_eq!(json!(null), Json::Null);
 }
